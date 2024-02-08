1221ac04ab7868fd3ff601d8d6a6fc6eb52e8667
Berryice <orecon0406@gmail.com>
2024-02-08 21:06:13 +0500

    3 prac

---
+++ TSI/3 Prac.cs
@@ -0,0 +1,68 @@
+﻿using System;
+public class App
+{
+    public static void Main(String[] args)
+    {
+        //Задание 1
+        Console.WriteLine("Введите сумму вклада");
+        double sum = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите колво месецев");
+        double mon = Convert.ToDouble(Console.ReadLine());
+        for (int i = 0; i < mon; ++i)
+        {
+            sum += ((sum / 100) * 7);
+            if (i == mon - 1)
+            {
+                Console.WriteLine($"Месяц {mon}: сумма вклада {sum}");
+            }
+        }
+        ///Задание 2
+        Console.Write("Введите Имя: ");
+        string name = Console.ReadLine();
+        Console.Write("Введите Фамилию: ");
+        string Sname = Console.ReadLine();
+        foreach (char element in name)
+        {
+            Console.WriteLine($"{element}");
+        }
+        foreach (char element in Sname)
+        {
+            Console.WriteLine($"{element}");
+        }
+        //Задание 3
+        Console.WriteLine("Введие число");
+        int i = 0, counter = 0;
+        Console.Write("Введите что-то ");
+        string number = Console.ReadLine();
+        do
+        {
+            if (char.IsNumber(number[i]))
+            {
+                counter += 1;
+            }
+            i += 1;
+        } while (i < number.Length);
+        Console.WriteLine($"В стоке {counter} цифр");
+        //Задание 4
+        Console.Write("Введите Число ");
+        int fakt = Convert.ToInt32(Console.ReadLine());
+        int end = 1;
+        for (int i = 1; i <= fakt; ++i)
+        {
+            end = end * i;
+        }
+        Console.WriteLine($"!{fakt} = {end}");
+        //Задание 5
+        int n = 20, m = 10;
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write("\n" + i + "\t");
+            for (int j = 1; j <= m; j++)
+            {
+                if (i > 0) Console.Write(i * j + "\t");
+                else Console.Write(j + "\t");
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ TSI/Тупицын 3.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Channels;
+
+public class App
+{
+    public static void Main(String[] args)
+    {
+        //Задание 1
+        Console.WriteLine("Введите сумму вклада");
+        double sum = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите колво месецев");
+        double mon = Convert.ToDouble(Console.ReadLine());
+        for (int i = 0; i < mon; ++i)
+        {
+            sum +=((sum/100)*7);
+            if (i == mon-1)
+            {
+                Console.WriteLine(sum);
+            }
+        }
+        //Задание 2
+        Console.WriteLine("Введите Имя");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите Фамилию");
+        string Sname = Console.ReadLine();
+        foreach (char element in name)
+        {
+            Console.WriteLine($"{element}");
+        }
+        foreach (char element in Sname)
+        {
+            Console.WriteLine($"{element}");
+        }
+        //Задание 3
+        Console.WriteLine("Введие число");
+        string value = Console.ReadLine();
+        int h = 0;
+        foreach(int j in value)
+        {
+            ++h;
+        }
+        Console.WriteLine(h);
+        //Задание 4
+        Console.Write("Введите Число ");
+        int fakt = Convert.ToInt32(Console.ReadLine());
+        int end = 1;
+        for(int i = 1; i<=fakt; ++i)
+        {
+            end = end*i;
+        }
+        Console.WriteLine(end);
+        //Задание 5
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.Write("\n"+i + "\t");
+            for (int j = 1; j <= 9; j++)
+            {
+                if (i > 0) Console.Write(i * j + "\t");
+                else Console.Write(j + "\t");
+            }
+            Console.Write("\n");
+        }
+    }
+}
